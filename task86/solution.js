58c196b027fe2824606047c4d063f5ccd9f1d0b1
Samuel Osoba <samuelsontech@gmail.com>
2022-05-19 18:41:55 +0100

    Task86 solved!

--- task86/solution.js
+++ task86/solution.js
@@ -1,6 +1,6 @@
-let name = 'Hero';
-let greeting = 'Good Day!'
-let count = 0;
+const name = "Hero";
+const greeting = "Good Day!";
+const count = 0;
 
 count = count + 1;
 console.log(greeting + " " + name + ". Count is " + count);
