35dc70ed9a835140ba5faf0272460b593e412a61
Samuel Osoba <samuelsontech@gmail.com>
2022-05-19 20:39:03 +0100

    Task99 successfully done!

--- task99/constants.js
+++ task99/constants.js
@@ -1,1 +1,3 @@
 // add constants
+export const rideSpeed = 10;
+export const rideTime = 23;
--- task99/solution.js
+++ task99/solution.js
@@ -1,1 +1,5 @@
 //add imports and console.log
+
+import { rideSpeed, rideTime } from "./constants.js";
+const distance = rideSpeed * rideTime;
+console.log(distance);
