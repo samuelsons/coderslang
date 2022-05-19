7146ef84151391aac172822095489fc63e22b752
Samuel Osoba <samuelsontech@gmail.com>
2022-05-19 18:38:39 +0100

    Task85 completed!

--- task85/solution.js
+++ task85/solution.js
@@ -2,6 +2,6 @@ function printUppercaseMessage(message) {
   console.log(message.toUpperCase());
 }
 
-let myMessage = `I'm on a path to becoming a JS developer!`;
+let myMessage = `I'm on a path to becoming a Javascript developer!`;
 
 printUppercaseMessage(myMessage);
